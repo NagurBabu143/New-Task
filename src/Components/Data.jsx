a652a8f925c25fdbe55250f0627b6c138fb5024d
NagurBabu143 <shaiknagurbabu00@gmail.com>
2025-02-24 10:16:27 +0530

    text file

---
+++ src/Components/Data.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Data extends Component {
+  render() {
+    return (
+      <div>
+        Shaik NagurBbau
+      </div>
+    )
+  }
+}
+
+export default Data
---
+++ src/Components/task.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function task() {
+  return (
+    <div>
+      <h1>Hi Hello</h1>
+    </div>
+  )
+}
+
+export default task
