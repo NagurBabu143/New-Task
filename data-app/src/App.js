38e3f2f9952ff3e36a615d7933932f5339d38ad5
NagurBabu143 <shaiknagurbabu00@gmail.com>
2025-02-24 08:39:14 +0530

    message

---
+++ data-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Data from './Components/Data';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Nagurbabu </h1>
+      <Data/>
+    </div>  
+  );
+}
+
+export default App;
