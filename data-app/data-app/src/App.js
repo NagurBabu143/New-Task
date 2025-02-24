e338f46ab0937eb4c01bebe476a5286fd96ba1d0
NagurBabu143 <shaiknagurbabu00@gmail.com>
2025-02-24 10:39:55 +0530

    Reinstalled Git and updated files

---
+++ data-app/data-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Data from './Components/Data';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Nagurbabu </h1>
+      <h2>update</h2>
+      <Data/>
+    </div>  
+  );
+}
+
+export default App;
